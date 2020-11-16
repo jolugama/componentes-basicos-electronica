8448dd0b55897246875b1f9ba843343b8ac6bf9c
Jose Luis G <garciamartinezjoseluis@gmail.com>
2020-11-16 22:15:48 +0100

    pantalla oled

---
+++ pantalla-oled-ssd1306/include/effectExamples.h
@@ -0,0 +1,116 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+
+namespace effectExamples {
+#define NUMFLAKES 10  // Number of snowflakes in the animation example
+
+#define LOGO_HEIGHT 16
+#define LOGO_WIDTH 16
+static const unsigned char PROGMEM logo_bmp[] = {
+    B00000000, B11000000,
+    B00000001, B11000000,
+    B00000001, B11000000,
+    B00000011, B11100000,
+    B11110011, B11100000,
+    B11111110, B11111000,
+    B01111110, B11111111,
+    B00110011, B10011111,
+    B00011111, B11111100,
+    B00001101, B01110000,
+    B00011011, B10100000,
+    B00111111, B11100000,
+    B00111111, B11110000,
+    B01111100, B11110000,
+    B01110000, B01110000,
+    B00000000, B00110000};
+
+#define XPOS 0  // Indexes into the 'icons' array in function below
+#define YPOS 1
+#define DELTAY 2
+
+// muestra texto con scroll.
+void testscrolltext(void) {
+    display.clearDisplay();
+
+    display.setTextSize(4);  // Draw 2X-scale text
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(300, 0);
+    display.println(F("JoLuGaMa"));
+    display.display();  // Show initial text
+    delay(100);
+
+    // Scroll in various directions, pausing in-between:
+    display.startscrollright(0x00, 0x0F);
+    delay(2000);
+    display.stopscroll();
+    delay(1000);
+    display.startscrollleft(0x00, 0x0F);
+    delay(2000);
+    display.stopscroll();
+    delay(1000);
+    display.startscrolldiagright(0x00, 0x07);
+    delay(2000);
+    display.startscrolldiagleft(0x00, 0x07);
+    delay(2000);
+    display.stopscroll();
+    delay(1000);
+}
+
+// función de muestra para dibujo. no la uso en el ejemplo.
+void testdrawbitmap(void) {
+    display.clearDisplay();
+
+    display.drawBitmap(
+        (display.width() - LOGO_WIDTH) / 2,
+        (display.height() - LOGO_HEIGHT) / 2,
+        logo_bmp, LOGO_WIDTH, LOGO_HEIGHT, 1);
+    display.display();
+    delay(1000);
+}
+
+// función de muestra de cómo se anima una imagen. No la uso en el ejemplo.
+void testanimate(const uint8_t *bitmap, uint8_t w, uint8_t h) {
+    int8_t f, icons[NUMFLAKES][3];
+
+    // Initialize 'snowflake' positions
+    for (f = 0; f < NUMFLAKES; f++) {
+        icons[f][XPOS] = random(1 - LOGO_WIDTH, display.width());
+        icons[f][YPOS] = -LOGO_HEIGHT;
+        icons[f][DELTAY] = random(1, 6);
+        Serial.print(F("x: "));
+        Serial.print(icons[f][XPOS], DEC);
+        Serial.print(F(" y: "));
+        Serial.print(icons[f][YPOS], DEC);
+        Serial.print(F(" dy: "));
+        Serial.println(icons[f][DELTAY], DEC);
+    }
+
+    for (;;) {                   // Loop forever...
+        display.clearDisplay();  // Clear the display buffer
+
+        // Draw each snowflake:
+        for (f = 0; f < NUMFLAKES; f++) {
+            display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, SSD1306_WHITE);
+        }
+
+        display.display();  // Show the display buffer on the screen
+        delay(200);         // Pause for 1/10 second
+
+        // Then update coordinates of each flake...
+        for (f = 0; f < NUMFLAKES; f++) {
+            icons[f][YPOS] += icons[f][DELTAY];
+            // If snowflake is off the bottom of the screen...
+            if (icons[f][YPOS] >= display.height()) {
+                // Reinitialize to a random position, just off the top
+                icons[f][XPOS] = random(1 - LOGO_WIDTH, display.width());
+                icons[f][YPOS] = -LOGO_HEIGHT;
+                icons[f][DELTAY] = random(1, 6);
+            }
+        }
+    }
+}
+
+}  // namespace effectExamples
